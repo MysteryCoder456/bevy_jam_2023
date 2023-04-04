6b284b91862734d2000864cd8518816ce562568f
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-04-04 13:06:36 +0400

    added game data resource loading

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*};
+use bincode::{Decode, Encode};
+use main_menu::MainMenuPlugin;
 
 #[cfg(debug_assertions)]
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_inspector_egui::quick::{ResourceInspectorPlugin, WorldInspectorPlugin};
 
 mod main_menu;
-
-use main_menu::MainMenuPlugin;
 
 #[derive(Component)]
 struct MainCamera;
@@ -24,6 +24,17 @@ struct UIAssets {
     button_pressed: Handle<Image>,
 }
 
+#[derive(Resource, Encode, Decode, Reflect)]
+struct GameData {
+    current_level: u32,
+}
+
+impl Default for GameData {
+    fn default() -> Self {
+        Self { current_level: 1 }
+    }
+}
+
 fn main() {
     let mut app = App::new();
 
@@ -36,10 +47,12 @@ fn main() {
     }))
     .add_state::<GameState>()
     .add_plugin(MainMenuPlugin)
-    .add_startup_systems((setup_camera, setup_assets));
+    .add_startup_systems((setup_camera, setup_assets, setup_game_data));
 
     #[cfg(debug_assertions)]
-    app.add_plugin(WorldInspectorPlugin::new());
+    app.add_plugin(WorldInspectorPlugin::new())
+        .register_type::<GameData>()
+        .add_plugin(ResourceInspectorPlugin::<GameData>::default());
 
     app.run();
 }
@@ -64,4 +77,20 @@ fn setup_assets(mut commands: Commands, asset_server: Res<AssetServer>) {
     };
 
     commands.insert_resource(ui_assets);
+}
+
+fn setup_game_data(mut commands: Commands) {
+    let game_data_path = "game_data.bin";
+    let config = bincode::config::standard();
+
+    let game_data: GameData = if let Ok(mut file) = std::fs::File::open(game_data_path) {
+        bincode::decode_from_std_read(&mut file, config).unwrap_or_default()
+    } else {
+        let default_game_data = GameData::default();
+        let encoded = bincode::encode_to_vec(&default_game_data, config).unwrap();
+        std::fs::write(game_data_path, &encoded).unwrap();
+        default_game_data
+    };
+
+    commands.insert_resource(game_data);
 }
